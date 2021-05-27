54a934317e458cc09549dec6d4e97c8c70ffb251
JonatanMagnusBakke <31619536+JonatanMagnusBakke@users.noreply.github.com>
2021-05-27 15:21:48 +0200

    Added neo4j webservice

---
+++ Microservices/Neo4j-Webservice/src/test/java/neo4j/neo4jwebservice/Neo4jwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package neo4j.neo4jwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
