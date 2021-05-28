0f2da9c2e4d6586cf67613925bf0d438c1cb9609
JonatanMagnusBakke <31619536+JonatanMagnusBakke@users.noreply.github.com>
2021-05-28 15:11:22 +0200

    You can now add recommendation that does everything for you

--- Microservices/Neo4j-Webservice/src/main/java/neo4j/neo4jwebservice/repository/MakeRepository.java
+++ Microservices/Neo4j-Webservice/src/main/java/neo4j/neo4jwebservice/repository/MakeRepository.java
@@ -1,10 +1,11 @@
 package neo4j.neo4jwebservice.repository;
 
-import java.util.List;
-
-import neo4j.neo4jwebservice.entities.Listing;
 import neo4j.neo4jwebservice.entities.Make;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MakeRepository extends Neo4jRepository<Make, Long>{
+    @Query("MATCH (p:Person {name:$username})-[:LIKES]->(make:Make {make:$make}) RETURN make;")
+    Make findMakeByUser(@Param("username") String username, @Param("make") String make);
 }
